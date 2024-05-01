c000ca42023bc30f4d8719d554942d56fcb66622
B00140738 <B00140738@mytudublin.ie>
2024-05-01 15:55:34 +0100

    update

---
+++ src/app/posts/[postId]/page.js
@@ -0,0 +1,13 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { Button, Box, TextField } from "@mui/material";
+import Layout from '../../Components/Layout';
+import '../../css/modulePage.css';
+import Comment from '../../Components/Comments';
+
+const CommentPage = () => {
+  <h1>It Works!</h1>
+};
+
+export default CommentPage;
